63fd2bfc239d5f5d6bcdc1172b115e12f4173c22
Henri <60694704+HNR1@users.noreply.github.com>
2023-05-23 17:32:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(rel_path):
 
 if __name__ == '__main__':
     setuptools.setup(
-        name='pytorch_fid',
+        name='pytorch-fid',
         version=get_version(os.path.join('src', 'pytorch_fid', '__init__.py')),
         author='Max Seitzer',
         description=('Package for calculating Frechet Inception Distance (FID)'
